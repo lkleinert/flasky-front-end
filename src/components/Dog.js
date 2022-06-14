5d1a89782b009aa2117cba8bb34d0f84ce245896
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-06-14 10:55:44 -0700

    Added Dog and DogList components

---
+++ src/components/Dog.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dog = () => {
+  return (
+    <li>
+      <h3>A soon to be Named Dog</h3>
+      <span>Chip #: 123 </span>
+      <button>add chip</button>
+    </li>
+  );
+};
+
+export default Dog;
