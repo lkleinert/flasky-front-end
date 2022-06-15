764edba170d698394e181bf9ce39d3a2b167d447
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-06-15 10:47:18 -0700

    Added props for Cat and CatList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import CatList from "./components/CatList";
 
+//place holder for future API call data
+const catData = [
+  {
+    name: "Whiskers",
+    chipNumber: "13579",
+  },
+
+  {
+    name: "Fang",
+    chipNumber: "82672",
+  },
+  {
+    name: "Beetle",
+    chipNumber: "02468",
+  },
+  {
+    name: "Loki",
+    chipNumber: "12345",
+  },
+];
+
 function App() {
   return (
     <main>
       <h1>The Cat Corral</h1>
-      <CatList></CatList>
+      <CatList cats={catData}></CatList>
     </main>
   );
 }
